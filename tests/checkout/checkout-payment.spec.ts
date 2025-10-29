6bce5e8b1754e9cc6825db67f2dfccdb97861180
Duat Le <duat.le@agest.vn>
2025-10-29 23:43:33 +0700

    tc4 completed - not tc3

--- tests/checkout/checkout-payment.spec.ts
+++ tests/checkout/checkout-payment.spec.ts
@@ -1,12 +1,5 @@
 import { test } from '../../fixtures/page.fixture'
-import { SortType } from '../../pages/shop.page'
 
 test("[TC_03] checkout: pays with direct bank transfer successfully", async ({ loggedInPage, shopPage }) => {
-    await loggedInPage.header.goToShop()
 
-    await shopPage.switchViewTo('List')
-    await shopPage.sortBy(SortType.PRICE_LOW_TO_HIGH)
-    await shopPage.shouldProductsBeSortedBy(SortType.PRICE_LOW_TO_HIGH)
-    await shopPage.sortBy(SortType.PRICE_HIGH_TO_LOW)
-    await shopPage.shouldProductsBeSortedBy(SortType.PRICE_HIGH_TO_LOW)
 })
